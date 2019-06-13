468be2b9fc724a4cbbd970814238926f4f476298
mortarsynth <epirtochol@gmail.com>
2019-06-13 16:29:40 +0300

    added swappers

--- include/matrix.h
+++ include/matrix.h
@@ -5,47 +5,47 @@
 
 class Matrix
 {	
-	private:		
+private:		
 
-		unsigned rows, cols;
-		float **matrix;
-	
-	public:
-	
-		Matrix(unsigned, unsigned, float**);
-		Matrix(unsigned, unsigned, const float&);
-		Matrix(unsigned, unsigned);
-		Matrix(const Matrix&);
-		virtual ~Matrix();
-		unsigned getNumOfRows() const;
-		unsigned getNumOfCols() const;
-		void printMatrix() const;
+	unsigned rows, cols;
+	float **matrix;
 
-		//float& operator()(const unsigned&, const unsigned&);
-		const float& operator()(const unsigned&, const unsigned&) const; 
+public:
 
-		Matrix& operator=(const Matrix&);
-		Matrix operator+(const Matrix&);
-		Matrix& operator+=(const Matrix&);
-		Matrix operator-(const Matrix&);
-		Matrix& operator-=(const Matrix&);
-		Matrix operator*(const Matrix&);
-		Matrix& operator*=(const Matrix&);
+	Matrix(unsigned, unsigned, float**);
+	Matrix(unsigned, unsigned, const float&);
+	Matrix(unsigned, unsigned);
+	Matrix(const Matrix&);
+	virtual ~Matrix();
+	unsigned getNumOfRows() const;
+	unsigned getNumOfCols() const;
+	void printMatrix() const;
 
-		Matrix transpose();
+	//float& operator()(const unsigned&, const unsigned&);
+	const float& operator()(const unsigned&, const unsigned&) const; 
 
-		Matrix getRow(unsigned) const;
-		Matrix getCol(unsigned) const;
-		void setRow(unsigned, const Matrix&);
-		void setCol(unsigned, const Matrix&);
-		Matrix deleteRow(unsigned) const;
-		Matrix deleteCol(unsigned) const;
-		void swapRows(unsigned, unsigned);
-		void swapCols(unsigned, unsigned);
-		float determinant() const;
+	Matrix& operator=(const Matrix&);
+	Matrix operator+(const Matrix&);
+	Matrix& operator+=(const Matrix&);
+	Matrix operator-(const Matrix&);
+	Matrix& operator-=(const Matrix&);
+	Matrix operator*(const Matrix&);
+	Matrix& operator*=(const Matrix&);
 
-		void allocMem();
-		void deallocMem();
+	Matrix transpose();
+
+	Matrix getRow(unsigned) const;
+	Matrix getCol(unsigned) const;
+	void setRow(unsigned, const Matrix&);
+	void setCol(unsigned, const Matrix&);
+	Matrix deleteRow(unsigned) const;
+	Matrix deleteCol(unsigned) const;
+	void swapRows(unsigned, unsigned);
+	void swapCols(unsigned, unsigned);
+	float determinant() const;
+
+	void allocMem();
+	void deallocMem();
 };
 
 
