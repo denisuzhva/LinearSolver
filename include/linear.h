8b670b8f6c40dac07d3e803fc4580e874d5964cb
mortarsynth <epirtochol@gmail.com>
2019-06-25 09:36:04 +0300

    linear solver done

--- include/linear.h
+++ include/linear.h
@@ -1,3 +1,5 @@
+#ifndef LINEARSOLVER_H
+#define LINEARSOLVER_H
 #include "./matrix.h"
 #include "./vector.h"
 #include "./cusmath.h"
@@ -8,3 +10,7 @@ Vector operator*(const Matrix&, const Vector&);
 Vector operator*(const Vector&, const Matrix&);
 
 Vector solveLinEq(Matrix, Vector);
+
+
+
+#endif
